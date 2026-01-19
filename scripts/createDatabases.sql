21fe8f69c3cee6dd1a254f866e87105bf13ea219
TayEdwards <taylahedwards12@gmail.com>
2026-01-20 10:21:16 +1300

    Add files via upload

---
+++ scripts/createDatabases.sql
@@ -0,0 +1,22 @@
+/*
+    ==============================================================================================================
+	MySQL vs SQLserver
+    MySQL does not the same logical grouping that SQL server has. There for going to create dw_bronze, dw_silver and dw_gold to represent schemas which are
+    there own databases.
+    
+    ** DONT THINK DATAWAREHOUSE DB IS NEEDED HERE AS THERE IS NO HIERARCHY FOR SCHEMAS INSIDE OF DATAWAREHOUSE LIKE SQLSERVER WILL UPDATE IF NEEDED **
+    
+    ==============================================================================================================
+    Script Purpose:
+		Checks first to see if there is a database with the same naming convention. If there isnt create the databases.
+        creates the databases which are three layers of the medalion design pattern bronze layer, silver layer , gold layer.
+*/
+
+-- CREATE DATABASE IF NOT EXISTS DataWareHouse; dont think you are needed bud --
+-- DROP DATABASE DataWareHouse; --
+CREATE DATABASE IF NOT EXISTS dw_bronze;
+CREATE DATABASE IF NOT EXISTS dw_silver;
+CREATE DATABASE IF NOT EXISTS dw_gold;
+
+
+
